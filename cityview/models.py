7db386615544ae7afd46ba37facfc5c8dd4ac97f
Nathan Sculli <scull7@gmail.com>
2013-07-09 00:39:14 -0700

    Re order model definitions for clarity.

--- cityview/models.py
+++ cityview/models.py
@@ -2,21 +2,31 @@ from django.db import models
 
 
 # Create your models here.
-class City(models.Model):
-    name = models.CharField('Name', max_length=100)
-    latitude = models.FloatField('Latitude')
-    longitude = models.FloatField('Longitude')
-    url = models.URLField('City URL')
-    county = models.ForeignKey('County')
+class State(models.Model):
+    abbreviation = models.CharField('Abbreviation', max_length=2)
+    name = models.CharField('Name', max_length=30)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.name, self.abbreviation)
 
 
 class County(models.Model):
     name = models.CharField('Name', max_length=100)
     full_name = models.CharField('Full Name', max_length=150)
     fips_code = models.PositiveIntegerField('FIPS Code')
-    state = models.ForeignKey('State')
+    state = models.ForeignKey(State)
+
+    def __unicode__(self):
+        return self.name
 
 
-class State(models.Model):
-    abbreviation = models.CharField('Abbreviation', max_length=2)
-    name = models.CharField('Name', max_length=30)
+class City(models.Model):
+    name = models.CharField('Name', max_length=100)
+    latitude = models.FloatField('Latitude')
+    longitude = models.FloatField('Longitude')
+    url = models.URLField('City URL')
+    county = models.ForeignKey(County)
+
+    def __unicode__(self):
+        return self.name
+
